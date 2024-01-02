c07fa0eaff7ee094cd1d7e8c57a40ab2890d09fb
YehudaGurovich <53883366+YehudaGurovich@users.noreply.github.com>
2024-01-02 23:36:06 +0200

    Point tests added

--- unittests/primitives/PointTest.java
+++ unittests/primitives/PointTest.java
@@ -8,10 +8,35 @@ class PointTest {
 
     @Test
     void testSubtract() {
+
+        Point point1 = new Point(2, 2, 2);
+        Point point2 = new Point(1, 1, 1);
+
+        Vector result = point1.subtract(point2);
+
+        assertEquals(result, new Vector(1, 1, 1));
     }
 
     @Test
+    void testSubtractResultsZero() {
+
+        Point point = new Point(1, 2, 3);
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> point.subtract(point));
+
+        // Assert the exception message if needed
+        assertEquals("Can't subtract a point from itself", exception.getMessage());
+    }
+
+    // TODO: Check for P(1, 1, 1).add(V(-1, -1, -1)) ???
+    @Test
     void testAdd() {
+        Point point = new Point(1, 2, 3);
+        Vector vector = new Vector(3, 2, 1);
+
+        Point point_result = point.add(vector);
+
+        assertEquals(point_result, new Point(4, 4, 4));
     }
 
 
