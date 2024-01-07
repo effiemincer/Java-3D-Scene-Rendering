34223acb0bd55f1a1a5cc1aadeee4feb35a79a16
Effie Mincer <effie.mincer@gmail.com>
2024-01-07 14:03:28 +0200

    added 1 test for tube

--- unittests/geometries/TubeTest.java
+++ unittests/geometries/TubeTest.java
@@ -1,6 +1,7 @@
 package geometries;
 
 import org.junit.jupiter.api.Test;
+import primitives.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,14 +9,18 @@ class TubeTest {
 
     @Test
     void testGetNormal() {
+        //TODO: I don't know how to calculate the normal of a tube
         // ============ Equivalence Partitions Tests ==============
-        //TC01: testing the calculation for getNormal
 
-        //Todo
+        Ray axis = new Ray(new Point(0,0,0), new Vector(1,0,0));
+        double radius = 1;
+        Tube tube = new Tube(radius, axis);
+
+        //TC01: testing the calculation for getNormal
+        assertEquals(tube.getNormal(new Point(0,1,0)), (new Vector(0,1,0)).normalize(), "ERROR: calculation of normal is incorrect");
 
         // =============== Boundary Values Tests ==================
-        //TC10: not exactly sure see instructions
-
+        //TC10: when the connection between the point on the body and the ray’s head creates a 90 degrees with the ray
         //Todo
     }
 }
