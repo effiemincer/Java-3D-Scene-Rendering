f9010344365895678f6429e56c6c1cd9bc8f8174
YehudaGurovich <53883366+YehudaGurovich@users.noreply.github.com>
2024-01-26 16:43:38 +0200

    IW test

--- unittests/renderer/ImageWriterTest.java
+++ unittests/renderer/ImageWriterTest.java
@@ -1,6 +1,7 @@
 package renderer;
 
 import org.junit.jupiter.api.Test;
+import primitives.Color;
 import scene.Scene;
 
 public class ImageWriterTest {
@@ -8,12 +9,26 @@ public class ImageWriterTest {
     void testImage() {
 //        Scene s = new Scene("Testing");
 //        Background colors and bg color for grid
+        Color lines = new Color(100, 100, 100);
+        Color bg = new Color(0, 0, 0);
         ImageWriter iw = new ImageWriter("Test image", 500, 800);
 
-            for (int i = 0; i < iw.getNx(); i++) {
-                for (int j = 0; j < iw.getNy(); j++) {
-                    iw.writePixel(i, j, );
-                }
+        for (int i = 0; i < iw.getNx(); i++) {
+            for (int j = 0; j < iw.getNy(); j++) {
+                iw.writePixel(i, j, bg);
+            }
+
+        }
+        for (int i = 0; i < iw.getNx(); i += 50) {
+            for (int j = 0; j < iw.getNy(); j++) {
+                iw.writePixel(i, j, lines);
+            }
+
+        }
+        for (int i = 0; i < iw.getNx(); i++) {
+            for (int j = 0; j < iw.getNy(); j += 50) {
+                iw.writePixel(i, j, lines);
+            }
 
         }
     }
