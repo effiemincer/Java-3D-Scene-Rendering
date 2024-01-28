22a736a2c71e6a85cf4f1cb884d7ee45d53f6766
YehudaGurovich <53883366+YehudaGurovich@users.noreply.github.com>
2024-01-28 17:36:46 +0200

    Minor format fixes and comments

--- unittests/renderer/ImageWriterTest.java
+++ unittests/renderer/ImageWriterTest.java
@@ -10,25 +10,23 @@ public class ImageWriterTest {
      */
     @Test
     void writeImageTest() {
-//        Scene s = new Scene("Testing");
-//        Background colors and bg color for grid
         Color lines = new Color(0, 0, 225);
-        Color bg = new Color(0,225, 0);
+        Color bg = new Color(0, 225, 0);
         ImageWriter iw = new ImageWriter("Test image", 800, 500);
 
-        //background colouring
+        // background colouring
         for (int i = 0; i < iw.getNx(); i++) {
             for (int j = 0; j < iw.getNy(); j++) {
                 iw.writePixel(i, j, bg);
             }
         }
-        //vertical line colouring
+        // vertical line colouring
         for (int i = 0; i < iw.getNx(); i += 50) {
             for (int j = 0; j < iw.getNy(); j++) {
                 iw.writePixel(i, j, lines);
             }
         }
-        //horizontal line colouring
+        // horizontal line colouring
         for (int i = 0; i < iw.getNx(); i++) {
             for (int j = 0; j < iw.getNy(); j += 50) {
                 iw.writePixel(i, j, lines);
