68e8e4cbbf098311a386c69947922b1303de5805
Effie Mincer <effie.mincer@gmail.com>
2024-02-25 19:25:45 +0200

    Tests for Glossy and Diffuse

--- unittests/renderer/GlossyDiffuseTest.java
+++ unittests/renderer/GlossyDiffuseTest.java
@@ -32,25 +32,50 @@ public class GlossyDiffuseTest {
             .setRayTracer(new SimpleRayTracer(scene));
 
     @Test
-    public void testGlossyDiffuse(){
+    public void testGlossy(){
         scene.geometries.add(
                 new Sphere(30d, new Point(0, 0, -11)) //
                         .setEmission(new Color(BLUE)) //
                         .setMaterial(new Material().setKd(0.5).setKs(0.5).setKr(1).setShininess(100)),
                 new Sphere(15d, new Point(25,40, 150))
                         .setEmission(new Color(150, 0, 0))
-                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(10))//
-//                new Triangle(new Point(  0,  85, 300),
-//                        new Point(  85, -85, -150),
-//                        new Point( -85, -85, -150))
-//                        .setEmission(new Color(0, 100, 0))
-//                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(100).setGlossDiffuse(0))
+                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(10)),//
+                new Triangle(new Point(  0,  -40, 300),
+                        new Point(  100, -40, -150),
+                        new Point( -100, -40, -150))
+                        .setEmission(new Color(0, 100, 0))
+                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(100))
         );
         scene.setAmbientLight(new AmbientLight(new Color(WHITE), 0.15));
         scene.lights.add(
                 new DirectionalLight(new Color(WHITE), new Vector(-1, -1, -4)));
 
-        camera.setImageWriter(new ImageWriter("GlossyDiffuse", 600, 600)).setTotalRays(40)
+        camera.setImageWriter(new ImageWriter("GlossyTest", 600, 600)).setTotalRays(40)
+                .build()
+                .renderImage()
+                .writeToImage();
+    }
+
+    @Test
+    public void testDiffuse(){
+        scene.geometries.add(
+                new Sphere(30d, new Point(0, 0, -11)) //
+                        .setEmission(new Color(BLUE)) //
+                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setKr(1).setShininess(100).setGloss(10)),
+                new Sphere(15d, new Point(25,40, 150))
+                        .setEmission(new Color(150, 0, 0))
+                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(10)),//
+                new Triangle(new Point(  0,  85, 300),
+                        new Point(  85, -85, 150),
+                     new Point( -85, -85, 150))
+                        .setEmission(new Color(0, 100, 0))
+                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setKt(1))
+        );
+        scene.setAmbientLight(new AmbientLight(new Color(WHITE), 0.15));
+        scene.lights.add(
+                new DirectionalLight(new Color(WHITE), new Vector(-1, -1, -4)));
+
+        camera.setImageWriter(new ImageWriter("DiffuseTest", 600, 600)).setTotalRays(40)
                 .build()
                 .renderImage()
                 .writeToImage();
